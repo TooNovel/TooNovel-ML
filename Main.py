e00ddf829e16e20be1f2d971c870127a8e9c1063
YJU-KimJeongSu <103088862+YJU-KimJeongSu@users.noreply.github.com>
2023-06-16 13:25:20 +0900

    :sparkles: 스케쥴링을 통한 자동 모델 업데이트
    - 백그라운드의 scheduler를 이용하여 매일 오전 4시 모델 업데이트
    - 모델 업데이트 시에는 추천 기능이 작동되지 않고 대기하도록 변경

--- Main.py
+++ Main.py
@@ -4,21 +4,38 @@ import ModelGenerator
 import Recommender
 import FilterMessage
 import DatabaseToCsv
+from apscheduler.schedulers.background import BackgroundScheduler
+from pytz import timezone
+import time
 
 app = FastAPI()
+
+scheduler = BackgroundScheduler()
+scheduler.start()
 
 class Chat(BaseModel):
     message: str
 
+isUpdating = False
+
 @app.get("/recommend/{user_id}")
 async def recommender(user_id: int):
-    print("recommender : Received Message : " + str(user_id))
-    return Recommender.run(user_id)
+    global isUpdating
+
+    if (isUpdating):
+        time.sleep(1)
+        return await recommender(user_id)
+    else:
+        print("recommender : Received Message : " + str(user_id))
+        return Recommender.run(user_id)
 
 @app.put("/")
 async def update():
+    global isUpdating
+    isUpdating = True
     DatabaseToCsv.run()
     ModelGenerator.run()
+    isUpdating = False
     
 @app.post("/filter")
 async def messageFilter(chat: Chat):
@@ -26,3 +43,14 @@ async def messageFilter(chat: Chat):
     
     result = FilterMessage.run(chat.message)
     return result
+
+def modelUpdateSchedule():
+    global isUpdating
+    print('Model Updating By Scheduler')
+    isUpdating = True
+    DatabaseToCsv.run()
+    ModelGenerator.run()
+    isUpdating = False
+    print('Model Update Done')
+
+scheduler.add_job(modelUpdateSchedule, 'cron', hour=4, minute=0, timezone=timezone('Asia/Seoul'))
